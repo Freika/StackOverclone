6384f16610a94b9c4d523bc9dab38f57276b2224
Freika <iamfrey@gmail.com>
2015-04-06 23:02:24 +0600

    Test Answer#mark_as_solution method.

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 describe Answer do
+  let(:question)      { create(:question) }
+  let(:first_answer)  { create(:answer, question: question) }
+  let(:second_answer) { create(:answer, question: question) }
+  let(:third_answer)  { create(:answer, question: question) }
+
   it { should validate_presence_of :body }
   it { should belong_to(:question) }
+
+  it '#mark_as_solution updates is_solution attribute' do
+    first_answer.mark_as_solution
+    expect(Answer.find(first_answer.id).is_solution).to eq true
+  end
+
+  it '#mark_as_solution update others answers is_solution to be false' do
+    first_answer.mark_as_solution
+    expect(Answer.find(first_answer.id).is_solution).to eq true
+
+    third_answer.mark_as_solution
+
+    expect(Answer.find(third_answer.id).is_solution).to eq true
+    expect(Answer.find(first_answer.id).is_solution).to eq false
+    expect(Answer.find(second_answer.id).is_solution).to eq false
+
+  end
 end
