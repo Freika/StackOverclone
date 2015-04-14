12e5f696427171612bc3495949ce44d17a4848b4
Freika <iamfrey@gmail.com>
2015-04-14 23:55:08 +0600

    Add file uploader and tests.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   validates :title, :body, presence: true
   has_many :answers, dependent: :destroy
+  has_many :attachments
   belongs_to :user
 end
---
+++ spec/features/add_files_to_questions_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Add files to question' do
+  given(:user) { create(:user) }
+
+  background do
+    sign_in_with(user.email, user.password)
+    visit new_question_path
+  end
+
+  scenario 'User adds file to question' do
+    fill_in 'Title', with: 'Title'
+    fill_in 'Body', with: 'Body'
+    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'Create'
+
+    expect(page).to have_content 'rails_helper.rb'
+  end
+end
