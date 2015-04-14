7f626451bbd544b1cc830f7305d21bec1d962198
Freika <iamfrey@gmail.com>
2015-04-15 01:12:01 +0600

    Add attachments t oanswers.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,9 @@ class Answer < ActiveRecord::Base
   validates :body, presence: true
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachmentable
+
+  accepts_nested_attributes_for :attachments
 
   scope :order_by_solution, -> { order(is_solution: :desc) }
 
