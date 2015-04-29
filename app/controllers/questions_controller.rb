b6826c087c0a44eed9532ba60ca05dc381e6e267
Freika <iamfrey@gmail.com>
2015-04-29 21:51:50 +0600

    Add file uploading.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.attachments.build
   end
 
   def edit
@@ -53,6 +54,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, attachments_attributes: [:file])
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,4 +4,6 @@ describe Question do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:attachments) }
+  it { should accept_nested_attributes_for :attachments }
 end
